54b5e6194893793c8aa130a7422de1f9920f7c0e
Rico Littawa <ricolittawa030620@gmail.com>
2022-09-21 18:23:48 -0500

    update

--- Admin/connection.php
+++ Admin/connection.php
@@ -1,23 +1,13 @@
 <?php
-
-$conn = "";
-
-try {
-	$servername = "localhost:3306";
-	$dbname = "127.0.0.1";
-	$username = "root";
-	$password = "";
-
-	$conn = new PDO(
-		"mysql:host=$servername; dbname=127.0.0.1",
-		$username, $password
-	);
-	
-$conn->setAttribute(PDO::ATTR_ERRMODE,
-					PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
-	echo "Connection failed: " . $e->getMessage();
-}
+$serverName= "DESKTOP-S2EIV7U";
+$ConnectionOptions=[
+	"Database"=>"DemoDB",
+	"UID"=>"",
+	"PWD"=>""
+];
+$conn = sqlsrv_connect($serverName, $ConnectionOptions);
+if($conn == false)
+	die(print_r(sqlsvr_errors(),true));
+	else echo 'Connection Success';
 
 ?>
