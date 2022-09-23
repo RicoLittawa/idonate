2452f3b276944ea8dd38e473406340f02755561c
Rico Littawa <ricolittawa030620@gmail.com>
2022-09-23 11:00:22 -0500

    fix the register validations

--- Admin/login/signup.php
+++ Admin/login/signup.php
@@ -34,6 +34,13 @@
 					<span class="login100-form-title">
 						Register
 					</span>
+					<div class="wrap-input100 validate-input" data-validate = "Name is required">
+						<input class="input100" type="text" name="fName" placeholder="Full name">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Email">
