9eebc095e5645ea0006d168fd2aa2ee26528734c
Rico Littawa <ricolittawa030620@gmail.com>
2022-10-08 00:37:06 -0500

    add validations

--- Admin/donations.php
+++ Admin/donations.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-?>
+
+	?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +9,6 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
-	
-
 	<!-- Boxicons -->
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<!-- My CSS -->
@@ -124,53 +122,54 @@ session_start();
 
       <!-- Modal body -->
      
-		<form action="../Admin/include/add.inc.php" method="POST" id="addmodalform">
+		
 		<div class="modal-body">
+		<form action="../Admin/include/add.inc.php" class="validate-form" method="POST">
 			
 	  					<input type="hidden" name="update_id">
 	  					<div class="form-group validate-input" data-validate = "Fullname is required">
 							<input class="form-control" type="text" name="fname" id="Ufullname" placeholder="Full name">
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group validate-input" data-validate = "Address is required">
 							<input class="form-control" type="text" name="address" id="Uaddress" placeholder="Address">
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 							<input class="form-control" type="text" name="email" id="Uemail"  placeholder="Email">
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group validate-input" data-validate = "Date is required">
 							<input class="form-control" type="date" name="donation_date" id="Udonation_date" placeholder="Date">
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group validate-input" data-validate = "Please select to the given options">
 								<label for="items">Select:</label>
-								<select class="form-control"  name="category" id="Ucategory">
+								<select class="form-control"  name="category" id="Ucategory" class="required">
+								<option value="">Choose...</option>
 								<option value="food">Food</option>
 								<option value="clothes">Clothes</option>
 								<option value="beverages">Beverages</option>
 								<option value="others">Others</option>
 								</select>
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group validate-input" data-validate = "Please select to the given options">
 							<label for="quanti">Select:</label>
-								<select class="form-control"  name="variant" id="Uvariant">
+								<select class="form-control"  name="variant" id="Uvariant" class="required">
+								<option value="">Choose...</option>
 								<option value="Per Box">Per Box</option>
 								<option value="Pieces">Pieces</option>
 								<option value="Others">Others</option>
 								</select>
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group validate-input" data-validate = "Product name is required">
 							<input class="form-control" type="text" id="UproductName" name="productName"placeholder="Product Name">
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group validate-input" data-validate = "This field is required">
 							<input class="form-control" type="number" id="Uquantity" name="quantity" placeholder="Quantity">
-						</div>
-
-       
+						</div>     
       </div>
 
       <!-- Modal footer -->
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-		<button type="submit" class="btn btn-primary submitBtn" onclick="submitdonations()">Save</button>
+		<button type="submit" name="submit-donations" class="btn btn-primary submitBtn">Save</button>
       </div>
 	  </form>	
 
@@ -219,20 +218,16 @@ session_start();
 				<td> <?php echo $row['variant']; ?></td>
 				<td> <?php echo $row['productName']; ?></td>
 				<td> <?php echo $row['quantity']; ?></td>
-				<td><a href="../admin/operations/delete.php?deleteid=<?php echo $row['id'] ?>"><i class="fa-solid fa-trash " style="color: red;"></i>
+				<td><a  class="btnDel" href="../admin/operations/delete.php?deleteid=<?php echo $row['id'] ?>"><i class="fa-solid fa-trash " style="color: red;"></i>
 				</a>
 				<button type="button" class="btn update"><i class="fa-solid fa-pen-to-square" style="color: green;"></i></button>
 					</td>
 
-				<td><button class="btn btn-success" type="submit">Send Certificate</button></td>
-					
+				<td><button class="btn btn-success" type="submit">Send Certificate</button></td>			
 			</tr>
-		
-	
 			<?php
 	  }
-	  ?>
-	  
+	  ?>  
 	  
     </tbody>
 	
@@ -250,19 +245,19 @@ session_start();
 	  <form action="../Admin/operations/update.php?" method="POST">
 		<div class="modal-body">
 	  					<input type="hidden" name="update_id" id="update_id">
-	  					<div class="form-group" data-validate = "">
+	  					<div class="form-group" data-validate = "Fullname is required">
 							<input class="form-control" type="text" name="fname" id="fname" placeholder="Full name" >
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group" data-validate = "Address is required">
 							<input class="form-control" type="text" name="address" id="address" placeholder="Address">
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group" data-validate = "Valid email is required: ex@abc.xyz">
 							<input class="form-control" type="text" name="email" id="email" placeholder="Email">
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group" data-validate = "Please select to the given options">
 								<label for="items">Select:</label>
-								<select class="form-control" id="category" name="category">
-								<option selected>Choose...</option>	
+								<select class="form-control" id="category" name="category" class="required">
+								<option value="">Choose...</option>
 								<option value="food">Food</option>
 								<option value="clothes">Clothes</option>
 								<option value="beverages">Beverages</option>
@@ -270,20 +265,20 @@ session_start();
 								</select>
 								<input class="editOption" style="display:none;"></input>
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group" data-validate = "Please select to the given options">
 							<label for="quanti">Select:</label>
-								<select class="form-control" id="variant" name="variant">
-								<option selected>Choose...</option>
+								<select class="form-control" id="variant" name="variant" class="required">
+								<option value="">Choose...</option>
 								<option value="Per Box">Per Box</option>
 								<option value="Pieces">Pieces</option>
 								<option class="editable" value="Others">Others</option>
 								</select>
 						</div>
-						<div class="form-group" data-validate = "">
+						<div class="form-group" data-validate = "Product name is required">
 							<input class="form-control" type="text" name="productName" id="productName" placeholder="Product Name">
 						</div>
-						<div class="form-group" data-validate = "">
-							<input class="form-control" type="number" name="quantity" id="quantity" placeholder="Quantity">
+						<div class="form-group" data-validate = "This field is required">
+							<input class="form-control" type="text" name="quantity" id="quantity" placeholder="Quantity">
 						</div>
 
        
@@ -292,7 +287,7 @@ session_start();
       <!-- Modal footer -->
       <div class="modal-footer">
         <button type="button" class="delete btn btn-danger" data-dismiss="modal">Close</button>
-		<button type="submit" name="updatedata" id="updatedata" class="btn btn-primary">Save</button>
+		<button type="submit" name="updatedata" id="updatedata" class="btn btn-primary updatedata">Save</button>
       </div>
 	  
 	  </form>	
@@ -309,13 +304,15 @@ session_start();
 	
 	
 
-	<script src="../Admin/scripts/donations.js"></script>
+	<script src="../Admin/scripts/landing.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.1.0.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+	<script src="../Admin/scripts/validation.js"></script>
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 
 
 	<script>
@@ -347,12 +344,66 @@ session_start();
 			$('.adddata').on('click',function(){
 				$('#addform').modal('show');
 
-
 			});
-
-
 		});
+		
+	</script>
+	<script>
+		$('.btnDel').on('click',function(e){
+			e.preventDefault();
+			const href=$(this).attr('href');
+						Swal.fire({
+				title: 'Are you sure?',
+				text: "You won't be able to revert this!",
+				icon: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#3085d6',
+				cancelButtonColor: '#d33',
+				confirmButtonText: 'Yes, delete it!'
+				}).then((result) => {
+				if (result.value) {
+					document.location.href=href;
+				}
+				})
+		})			
+
+	</script>
+
+
+		
+		<?php
+		
+	if (isset($_SESSION['status']) && $_SESSION['status']!='')
+	{?>
+	<script>
+		swal({  
+  		title: " Confirm?",  
+ 		 text: "<?php echo $_SESSION['status']; ?>",  
+ 		 type: "<?php echo $_SESSION['status_code']; ?>",  
+ 		 showCancelButton: true,  
+		confirmButtonClass: "btn-danger",  
+		confirmButtonText: " Confirm, remove it!",  
+		closeOnConfirm: false  
+		},  
+		function(){  
+		swal("remove it!", "Imaginary file remove it.", "Confirm");  
+		})
+		
+	</script>
+	<script>
+			Swal.fire({
+			icon: '<?php echo $_SESSION['status_code']; ?>',
+			title: '<?php echo $_SESSION['status']; ?>',
+			button:"ok, done",
+			})
 	</script>
 	
+	<?php
+		unset($_SESSION['status']);
+	}
+	?>
+
+
+
 </body>
 </html>
