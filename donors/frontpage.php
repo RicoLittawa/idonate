d235524cb67a641de87ff01719e1b7e6bd22f02e
Rico Littawa <ricolittawa030620@gmail.com>
2022-10-09 17:08:54 -0500

    update donors

--- donors/frontpage.php
+++ donors/frontpage.php
@@ -5,6 +5,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&family=Kantumruy+Pro:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/donation.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
@@ -22,17 +24,12 @@
           <li class="nav-item">
             <a class="nav-link active" href="#">Home</a>
           </li>
-
-
           <li class="nav-item">
             <a class="nav-link" href="donation.php">Donations</a>
           </li>
-
-
           <li class="nav-item">
             <a class="nav-link" href="howitworks.php">How it works?</a>
           </li>
-
           <li class="nav-item">
             <a class="nav-link" href="">What is needed?</a>
           </li>
@@ -76,14 +73,14 @@
           disaster risk reduction in Batangas City led by Mayor Beverly Dimacuha. Their office is 
           located in barangay Bolbok, Batangas City. Their mission is to reduce risk and vulnerabilities, mitigate the 
           negative effects of hazards, and assure Batanguenos' safety and security. .... </p>
-         
+          <button type="button" class="btn btn-primary">Read more></button>
       </div>
       <footer class="site-footer">
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-6">
             <h6>About</h6>
-            <p class="text-justify" style="font-family: 'Lato', sans-serif;">IDONATE is an online platform created for CDRRMO. With the use of the internet, 
+            <p class="text-justify">IDONATE is an online platform created for CDRRMO. With the use of the internet, 
             this web-based system will lessen the manual process. It will also automate sending the acknowledgement receipt to the donors.
              Donors may see how their donations are used with the help of a web-based donation system.</p>
           </div>
@@ -116,10 +113,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
 window.onscroll = function() {myFunction()};
-
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
-
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
