33cf97b1d0822852a544fbada438c331c98f5a32
Mariestella Suarez <mariestella.suarez@g.batstate-u.edu.ph>
2022-10-09 22:33:10 +0800

    Fix navigation bar glich; Add font CDN;

--- donors/donation.php
+++ donors/donation.php
@@ -6,8 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&family=Kantumruy+Pro:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet">
-   
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&family=Kantumruy+Pro:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/donation.css">
@@ -19,6 +17,7 @@
     }</style>
   </head>
   <body> 
+
   <div class="navbar navbar-expand-sm bg-light" id="navbar">
   <ul class="nav nav-pills nav-fill">
           <li class="nav-item">
@@ -35,6 +34,7 @@
           </li>
         </ul>
 </div>
+
 <div class="container">
   <div class="circles">
         <div class="dot" >
