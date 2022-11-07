c00eb5e2c0be70fc5ca26df5133b4f886a236316
Mariestella B. Suarez <mariestella.suarez@g.batstate-u.edu.ph>
2022-11-07 19:54:59 +0800

    must trry

--- donors/howitworks.php
+++ donors/howitworks.php
@@ -12,9 +12,9 @@ session_start()?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/donation.css">
     <link rel="stylesheet" href="css/donors.css">
+    <link rel="stylesheet" href="css/howitworks.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link rel="stylesheet" href="howitworks.css">
     <style>body.modal-open, .modal-open .navbar-fixed-top, .modal-open .navbar-fixed-bottom {
         padding-right: 0px !important;
     }</style>
