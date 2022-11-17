b4324c6a43041881b3bedcacf0dc527f9972ef15
Mariestella Suarez <mariestella.suarez@g.batstate-u.edu.ph>
2022-11-18 02:22:58 +0800

    update

--- donors/donation.php
+++ donors/donation.php
@@ -37,7 +37,7 @@ session_start()?>
           <a class="nav-link" href="howitworks.php">How it works?</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="whatisneeded">What is needed?</a>
+          <a class="nav-link" href="whatisneeded.php">What is needed?</a>
         </li>
        
         
