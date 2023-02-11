9489e37a8f720d35b6d334c5f539da6af9fa36c5
Rico Littawa <rico.littawa.pixel8@gmail.com>
2023-02-11 22:27:25 +0800

    try

--- Admin/users.php
+++ Admin/users.php
@@ -15,7 +15,7 @@ $result= mysqli_query($conn,$sql);
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&family=Kantumruy+Pro:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 	<!-- Boxicons -->
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
@@ -23,11 +23,21 @@ $result= mysqli_query($conn,$sql);
 	<link rel="stylesheet" href="css/donations.css">
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap4.min.css">
 	<link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css">
+	<link rel="stylesheet" href="css/mdb.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-floating-labels@3.0.0/dist/css/bootstrap-floating-labels.min.css" />
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Material Design Bootstrap -->
+	<link rel="stylesheet" href="css/mdb.min.css">
+
+
+	<!-- Your custom styles (optional) -->
+
+	
+
 	<title>User Details</title>
 </head>
 <body>
-
-
 	<!-- SIDEBAR -->	
 	<section id="sidebar">
 		<a href="#" class="brand">
@@ -123,8 +133,7 @@ $result= mysqli_query($conn,$sql);
 						</li>
 					</ul>
 				</div>
-				<span><button class="btn adddata" id="toggleFormBtn" type="button" style=" width:200px;height:50px;"><i class="fas fa-add"></i>Show Form</button></span>
-
+				<span><button class="btn btn-success adddata" id="toggleFormBtn" type="button" style=" width:200px;height:50px;"><i class="fas fa-add"></i>Show Form</button></span>
 			</div>
 			<div class="add-user" id="add-user">
 				</div>
@@ -134,37 +143,83 @@ $result= mysqli_query($conn,$sql);
 						<h3>Account Details</h3>
 						</div>
 						
-						<div id="registerForm" style="display: none;">
-      <form>
-		<div class="form-group">
-		<input class="form-control" type="text" placeholder="Name"><br><br>
-		</div>
-        <input class="form-control" type="email" placeholder="Email"><br><br>
-        <input class="form-control" type="password" placeholder="Password"><br><br>
-        <input class="btn btn-success" type="submit" value="Submit">
-		
-        
-      </form>
+	<div id="registerForm" class="form-outline mb-3" style="display: none;">
+	<form>
+  <!-- 2 column grid layout with text inputs for the first and last names -->
+  <div class="row mb-4">
+    <div class="col">
+      <div class="form-outline">
+        <input type="text" id="fname" class="form-control" placeholder="Enter name" />
+        <label class="form-label" style="font-weight:800;" for="fname">First name</label>
+      </div>
     </div>
+    <div class="col">
+      <div class="form-outline">
+        <input type="text" id="form3Example2" class="form-control" />
+        <label class="form-label" for="form3Example2">Last name</label>
+      </div>
+    </div>
+  </div>
+	<div class="row mb-4">
+		<div class="col">
+		<div class="form-outline mb-4">
+		<select class="form-control" name="" id="">
+				<option value="">Choose</option>
+				<option value="">Admin</option>
+				<option value="">User</option>
+			</select>
+			<label class="form-label" for="form3Example3">Role</label>
+	</div>
+	</div>
+	<div class="col">
+ <!-- Email input -->
+ <div class="form-outline mb-4">
+    <input type="email" id="form3Example3" class="form-control" />
+    <label class="form-label" for="form3Example3">Email address</label>
+  </div>
+	</div>
+
+	</div>
+ 
+	
+  <!-- Password input -->
+  <div class="form-outline mb-4">
+    <input type="password" id="form3Example4" class="form-control" />
+    <label class="form-label" for="form3Example4">Password</label>
+  </div>
+
+  <!-- Submit button -->
+  <button type="submit" class="btn btn-success btn-block mb-4">Register</button>
+  <br><br>
+  <hr class="hr hr-blurry">
+    </div>
+	<div class="form-group">
+  <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  <label for="exampleInputEmail1">Email address</label>
+</div>
     <br><br>
-    <table class="table table-condensed">
+    <table  class="table table-striped table-bordered" style="width:100%" id="table_data">
       <thead>
         <tr>
           <th>Name</th>
           <th>Email</th>
-          <th>Password</th>
+          <th>Type</th>
+		  <th>Status</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>John Doe</td>
           <td>johndoe@example.com</td>
-          <td>12345</td>
+          <td>Admin</td>
+		  <td><span class="badge badge-primary">Active</span></td>
+	
         </tr>
         <tr>
           <td>Jane Doe</td>
           <td>janedoe@example.com</td>
-          <td>67890</td>
+          <td>User</td>
+		  <td><span class="badge badge-danger">Inactive</span></td>
         </tr>
       </tbody>
     </table>
@@ -178,11 +233,19 @@ $result= mysqli_query($conn,$sql);
 	<script src="scripts/sidemenu.js"></script>
 	<script src="scripts/jQuery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<script src="scripts/sweetalert2.all.min.js"></script>	
 	<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
   	<script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js"></script>
 	<script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
+	<script type="text/javascript" src="scripts/popper.min.js"></script>
+	<script type="text/javascript" src="scripts/mdb.min.js"></script>
+
+	
+
+
+
+
 	<script>
 	  $(document).ready(function () {
 			$('#table_data').DataTable({
@@ -199,9 +262,7 @@ $result= mysqli_query($conn,$sql);
 			});
 		});
 	</script>
-
-
-<script>
+	<script>
         $(document).ready(function(){
         $("#toggleFormBtn").click(function(){
           $("#registerForm").toggle();
@@ -213,6 +274,7 @@ $result= mysqli_query($conn,$sql);
         });
       });
     </script>
+
 
 </body>
 </html>
